a3d09165c90d3047d3b1936f861e095c21ae026f
Greg-design <gregory_gr@outlook.com>
2023-05-08 13:00:12 -0300

    toggle do icone de som

--- src/components/SliderSound/index.tsx
+++ src/components/SliderSound/index.tsx
@@ -1,12 +1,22 @@
-import { SoundFilled } from "@ant-design/icons";
+import { CloseCircleFilled, SoundFilled } from "@ant-design/icons";
 import { Button, Slider, Space } from "antd";
+import { useState } from "react";
 import style from "./style.module.css";
 
 const SliderSound = () => {
+  const [isMuted, setIsMuted] = useState(false);
+
+  const handleSliderChange = (value: number) => {
+    if (value === 0) {
+      setIsMuted(true);
+    } else {
+      setIsMuted(false);
+    }
+  };
   return (
     <Space>
-      <Button type="text" icon={<SoundFilled />} />
-      <Slider defaultValue={30} className={style.slider} />
+      <Button type="text" icon={isMuted ? <CloseCircleFilled /> : <SoundFilled />} />
+      <Slider defaultValue={30} className={style.slider} onChange={handleSliderChange} />
     </Space>
   );
 };
